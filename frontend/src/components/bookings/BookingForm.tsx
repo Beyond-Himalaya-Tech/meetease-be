d8a7a35a00fc8e0c76ae7b655a55d2e2b596f3e1
beyond_lakpa <lakpa_lama@beyondhimalayatech.com.au>
2025-12-29 16:55:36 +0545

    phone number validation added

--- frontend/src/components/bookings/BookingForm.tsx
+++ frontend/src/components/bookings/BookingForm.tsx
@@ -8,6 +8,7 @@ import type { EventInfoProps } from "../meeting/EventInfo";
 import { createEvent } from "../../lib/api";
 import toast from "react-hot-toast";
 import { tzMap } from "../../constants/timezones";
+import { rules } from "../../constants/phoneNumberRules";
 
 type Props = {
   event: EventInfoProps;
@@ -144,45 +145,6 @@ const BookingForm = ({ event, selectedDate, selectedTime, onBack, eventTypeId, t
       setPhoneError(null);
       return true;
     }
-
-    const rules: Record<
-      string,
-      {
-        min: number;
-        max: number;
-        startsWith?: RegExp;
-        message?: string;
-      }
-    > = {
-      "+977": {
-        min: 10,
-        max: 10,
-        startsWith: /^(97|98)/,
-        message: "Nepal numbers must start with 97 or 98",
-      },
-      "+91": {
-        min: 10,
-        max: 10,
-        startsWith: /^[6-9]/,
-        message: "India numbers must start with 6–9",
-      },
-      "+61": {
-        min: 9,
-        max: 9,
-        startsWith: /^4/,
-        message: "Australia numbers must start with 4",
-      },
-      "+44": {
-        min: 10,
-        max: 10,
-        startsWith: /^7/,
-        message: "UK mobile numbers must start with 7",
-      },
-      "+1": {
-        min: 10,
-        max: 10,
-      },
-    };
 
     const rule = rules[countryCode];
 
